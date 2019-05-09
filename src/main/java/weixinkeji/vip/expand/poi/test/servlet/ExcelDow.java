1236ad032f07e4f92a59ddd25b5735a93899a827
wangchunzi <pankeng1988w@aliyun.com>
2019-05-10 06:33:15 +0800

    1

--- src/main/java/weixinkeji/vip/expand/poi/test/servlet/ExcelDow.java
+++ src/main/java/weixinkeji/vip/expand/poi/test/servlet/ExcelDow.java
@@ -3,7 +3,9 @@ package weixinkeji.vip.expand.poi.test.servlet;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import weixinkeji.vip.expand.poi.JWEOfficeEnum;
 import weixinkeji.vip.expand.poi.JWEOfficeW;
+import weixinkeji.vip.expand.poi.JWEOfficeWebTool;
 import weixinkeji.vip.expand.poi.test.entity.User;
 
 public class ExcelDow extends HttpServlet {
@@ -22,13 +25,25 @@ public class ExcelDow extends HttpServlet {
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String sheet = req.getParameter("sheet");
 		if (sheet.equalsIgnoreCase("1")) {
-			this.doOneSheet(req, resp);
+//			this.doOneSheet(req, resp); 
+			//或 使用封装的工具
+			this.doOneSheetByWebTool(req, resp);
 		} else {
-			this.doManySheet(req, resp);
+//			this.doManySheet(req, resp);
+			//或 使用封装的工具
+			this.doManySheetByWebTool(req, resp);
 		}
 	}
-	
-	//单个工作表时
+
+	// 单个工作表时_使用封装的web工具类
+	private void doOneSheetByWebTool(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String sort = req.getParameter("sort");
+		JWEOfficeWebTool.downloadExcelFile(resp, this.getUser(),
+				null == sort || sort.equalsIgnoreCase("xls") ? "用户信息集合(单表).xls" : "用户信息集合(单表).xlsx", "用户信息", true);
+	}
+
+	// 单个工作表时
 	private void doOneSheet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String sort = req.getParameter("sort");
 		if (null == sort || sort.equalsIgnoreCase("xls")) {
@@ -40,7 +55,20 @@ public class ExcelDow extends HttpServlet {
 		}
 	}
 	
-	//多个工作表时
+	// 多个工作表时：使用封装的Web工具
+	private void doManySheetByWebTool(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String sort = req.getParameter("sort");
+		String fileName="用户信息集合(多表).xlsx";
+		if (null == sort || sort.equalsIgnoreCase("xls")) {
+			fileName="用户信息集合(多表).xls";
+		}
+		Map<String,List<User>> map=new HashMap<>();
+		map.put("用户信息", this.getUser());
+		map.put("用户信息2", this.getUser());
+		JWEOfficeWebTool.downloadExcelFile_autoClose(resp, map, fileName, true);
+	}
+	
+	// 多个工作表时
 	private void doManySheet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String sort = req.getParameter("sort");
 		if (null == sort || sort.equalsIgnoreCase("xls")) {
@@ -66,14 +94,16 @@ public class ExcelDow extends HttpServlet {
 
 		}
 	}
-	//设置下载时，文件名的显示 及编号 处理
-	private void setFileContentHeader(HttpServletResponse resp,String filename) throws IOException {
+
+	// 设置下载时，文件名的显示 及编号 处理
+	private void setFileContentHeader(HttpServletResponse resp, String filename) throws IOException {
 		resp.setHeader("Content-Disposition",
 				"attachment;filename=" + new String(filename.getBytes("utf-8"), "ISO8859-1"));
 	}
-	
+
 	/**
 	 * 模拟后台数据
+	 * 
 	 * @return
 	 */
 	private List<User> getUser() {
--- src/main/java/weixinkeji/vip/expand/poi/test/servlet/ExcelUpload.java
+++ src/main/java/weixinkeji/vip/expand/poi/test/servlet/ExcelUpload.java
@@ -2,6 +2,7 @@ package weixinkeji.vip.expand.poi.test.servlet;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +12,7 @@ import javax.servlet.http.Part;
 
 import weixinkeji.vip.expand.poi.JWEOfficeEnum;
 import weixinkeji.vip.expand.poi.JWEOfficeR;
+import weixinkeji.vip.expand.poi.JWEOfficeWebTool;
 import weixinkeji.vip.expand.poi.test.entity.User;
 
 public class ExcelUpload extends HttpServlet {
@@ -19,66 +21,88 @@ public class ExcelUpload extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	
+
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String sheet = req.getParameter("sheet");
 		if (sheet.equalsIgnoreCase("1")) {
-			this.readOne(req, resp);
+//			this.readOne(req, resp);
+//			或使用封装工具：
+			this.readOneByWebTool(req, resp);
 		} else {
-			this.readMany(req, resp);
+			//this.readMany(req, resp);
+//			或使用封装工具：
+			this.readManyByWebTool(req, resp);
 		}
+		resp.sendRedirect(req.getContextPath() + "/index.jsp");
 	}
+
+	//使用封装的工具
+	private void readOneByWebTool(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		List<User> list = JWEOfficeWebTool.getUploadExcelFile(req, "excelfilename", User.class);
+		this.prUser(list);
+	}
+	
+	//使用封装的工具
+	private void readManyByWebTool(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		Map<Integer,Object> map=JWEOfficeWebTool.getUploadExcelFile(req, "excelfilename", User.class,User.class);
+		for(Map.Entry<Integer,Object> kv:map.entrySet()) {
+			System.out.println("第"+kv.getKey()+"张表的数据：");
+			List<User> value = (List<User>)kv.getValue();
+			this.prUser(value);
+		}
+		
+	}
+	
+	
 	private void readOne(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		Part part=req.getPart("excelfilename");
+		Part part = req.getPart("excelfilename");
 		try {
-			System.out.println(part.getSubmittedFileName()+"//"+part.getName());
-			
-			List<User> list=null;
-			if(part.getSubmittedFileName().endsWith(".xls")) {
-				list=JWEOfficeR.readExcel_xls(User.class,"用户信息",part.getInputStream());
+			System.out.println(part.getSubmittedFileName() + "//" + part.getName());
+
+			List<User> list = null;
+			if (part.getSubmittedFileName().endsWith(".xls")) {
+				list = JWEOfficeR.readExcel_xls(User.class, "用户信息", part.getInputStream());
 			}
-			if(part.getSubmittedFileName().endsWith(".xlsx")) {
-				list=JWEOfficeR.readExcel_xlsx(User.class,"用户信息",part.getInputStream());
+			if (part.getSubmittedFileName().endsWith(".xlsx")) {
+				list = JWEOfficeR.readExcel_xlsx(User.class, "用户信息", part.getInputStream());
 			}
 			this.prUser(list);
-			
-			resp.sendRedirect(req.getContextPath()+"/index.jsp");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	private void readMany(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		Part part=req.getPart("excelfilename");
-		try {
-			System.out.println(part.getSubmittedFileName()+"//"+part.getName());
-			List<User> list=null;
-			if(part.getSubmittedFileName().endsWith(".xls")) {
-				JWEOfficeR obj=new JWEOfficeR(part.getInputStream(),JWEOfficeEnum.xls);
-				list=obj.readExcel(User.class, "用户信息");
-				this.prUser(list);
-				System.out.println("------------------------------------");
-				list=obj.readExcel(User.class, "用户信息2");
-				this.prUser(list);
-			}
-			if(part.getSubmittedFileName().endsWith(".xlsx")) {
-				JWEOfficeR obj=new JWEOfficeR(part.getInputStream(),JWEOfficeEnum.xlsx);
-				list=obj.readExcel(User.class, "用户信息");
-				this.prUser(list);
-				System.out.println("------------------------------------");
-				list=obj.readExcel(User.class, "用户信息2");
-				this.prUser(list);
-			}
-			resp.sendRedirect(req.getContextPath()+"/index.jsp");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	
+	private void readMany(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Part part = req.getPart("excelfilename");
+		try {
+			System.out.println(part.getSubmittedFileName() + "//" + part.getName());
+			List<User> list = null;
+			if (part.getSubmittedFileName().endsWith(".xls")) {
+				JWEOfficeR obj = new JWEOfficeR(part.getInputStream(), JWEOfficeEnum.xls);
+				list = obj.readExcel(User.class, "用户信息");
+				this.prUser(list);
+				System.out.println("------------------------------------");
+				list = obj.readExcel(User.class, "用户信息2");
+				this.prUser(list);
+			}
+			if (part.getSubmittedFileName().endsWith(".xlsx")) {
+				JWEOfficeR obj = new JWEOfficeR(part.getInputStream(), JWEOfficeEnum.xlsx);
+				list = obj.readExcel(User.class, "用户信息");
+				this.prUser(list);
+				System.out.println("------------------------------------");
+				list = obj.readExcel(User.class, "用户信息2");
+				this.prUser(list);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	private void prUser(List<User> list) {
-		for(User user:list) {
+		for (User user : list) {
 			System.out.println(user.toString());
 		}
 	}
